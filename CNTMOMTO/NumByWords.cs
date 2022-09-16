69953a392aee04fe9264e3200a5acd0d77afdb18
Звягин Евгений <eazvyagin@gmail.com>
2022-09-16 22:30:35 +0300

    Добавьте файлы проекта.

---
+++ CNTMOMTO/NumByWords.cs
@@ -0,0 +1,112 @@
+﻿
+    using System;
+    using Intermech;
+    using Intermech.DataFormats;
+    using Intermech.Interfaces;
+    using Intermech.Interfaces.Plugins;
+    using Intermech.Interfaces.Client;
+    using Intermech.Interfaces.Configuration;
+    using Intermech.Kernel.Search;
+    using Intermech.Navigator;
+
+    namespace CNTMOMTO
+    {
+    /// <summary>
+    /// Класс, который позволяет создать новый объект
+    /// </summary>
+    public class NumByWords
+    {
+
+        public string RurPhrase(decimal money)
+        {
+            return CurPhrase(money, "рубль", "рубля", "рублей", true, "копейка", "копейки", "копеек", false);
+        }
+
+        public string UsdPhrase(decimal money)
+        {
+            return CurPhrase(money, "доллар США", "доллара США", "долларов США", true, "цент", "цента", "центов", true);
+        }
+
+        public string NumPhrase(ulong Value, bool IsMale)
+        {
+            if (Value == 0UL) return "Ноль";
+            string[] Dek1 = { "", " од", " дв", " три", " четыре", " пять", " шесть", " семь", " восемь", " девять", " десять", " одиннадцать", " двенадцать", " тринадцать", " четырнадцать", " пятнадцать", " шестнадцать", " семнадцать", " восемнадцать", " девятнадцать" };
+            string[] Dek2 = { "", "", " двадцать", " тридцать", " сорок", " пятьдесят", " шестьдесят", " семьдесят", " восемьдесят", " девяносто" };
+            string[] Dek3 = { "", " сто", " двести", " триста", " четыреста", " пятьсот", " шестьсот", " семьсот", " восемьсот", " девятьсот" };
+            string[] Th = { "", "", " тысяч", " миллион", " миллиард", " триллион", " квадрилион", " квинтилион" };
+            string str = "";
+            for (byte th = 1; Value > 0; th++)
+            {
+                ushort gr = (ushort)(Value % 1000);
+                Value = (Value - gr) / 1000;
+                if (gr > 0)
+                {
+                    byte d3 = (byte)((gr - gr % 100) / 100);
+                    byte d1 = (byte)(gr % 10);
+                    byte d2 = (byte)((gr - d3 * 100 - d1) / 10);
+                    if (d2 == 1) d1 += (byte)10;
+                    bool ismale = (th > 2) || ((th == 1) && IsMale);
+                    // str = Dek3[d3] + Dek2[d2] + Dek1[d1] + EndDek1(d1, ismale) + Th[th] + EndTh(th, d1) + str;
+                    str = Dek3[d3] + Dek2[d2] + Dek1[d1] + EndDek1(d1, ismale) + Th[th] + EndTh(th, d1) + str;
+                };
+            };
+            str = str.Substring(1, 1).ToUpper() + str.Substring(2);
+            return str;
+        }
+
+        private string CurPhrase
+            (
+            decimal money,
+            string word1, string word234, string wordmore, bool IsMale,
+            string sword1, string sword234, string swordmore, bool sIsMale
+            )
+        {
+            money = decimal.Round(money, 2);
+            decimal decintpart = decimal.Truncate(money);
+            ulong intpart = decimal.ToUInt64(decintpart);
+            string str = NumPhrase(intpart, IsMale) + " ";
+            byte endpart = (byte)(intpart % 100UL);
+            if (endpart > 19) endpart = (byte)(endpart % 10);
+            byte fracpart = decimal.ToByte((money - decintpart) * 100M);
+            //  str += "и " + ((fracpart < 10) ? "0" : "") + fracpart.ToString() + "/100 ";
+
+            switch (endpart)
+            {
+                case 1: str += word1; break;
+                case 2:
+                case 3:
+                case 4: str += word234; break;
+                default: str += wordmore; break;
+            }
+            str += " и " + fracpart.ToString() + " коп.";
+            return str;
+        }
+
+        private static string EndTh(byte ThNum, byte Dek)
+        {
+            bool In234 = ((Dek >= 2) && (Dek <= 4));
+            bool More4 = ((Dek > 4) || (Dek == 0));
+            if (((ThNum > 2) && In234) || ((ThNum == 2) && (Dek == 1))) return "а";
+            else if ((ThNum > 2) && More4) return "ов";
+            else if ((ThNum == 2) && In234) return "и";
+            else return "";
+        }
+
+        private static string EndDek1(byte Dek, bool IsMale)
+        {
+            if ((Dek > 2) || (Dek == 0)) return "";
+            else if (Dek == 1)
+            {
+                if (IsMale) return "ин";
+                else return "на";
+            }
+            else
+            {
+                if (IsMale) return "а";
+                else return "е";
+            }
+        }
+
+    }
+}
+
---
+++ CNTMOMTO/OMTO.cs
@@ -0,0 +1,246 @@
+﻿
+namespace CNTMOMTO
+{
+
+using System;
+using System.Windows.Forms;
+
+using Intermech;
+using Intermech.Interfaces;
+using Intermech.Interfaces.Client;
+using Intermech.Interfaces.Plugins;
+using Intermech.Navigator.ContextMenu;
+using Intermech.Navigator.Interfaces;
+using Intermech.DataFormats;
+using Intermech.Interfaces.Projects;
+using Intermech.Interfaces.Workflow;
+
+  
+
+
+    public class PluginPackage : AbstractPackage
+    {
+        public PluginPackage()
+            : base("Закупки")
+        {
+        }
+
+        internal static IServiceProvider _serviceProvider;
+       // internal static dynamic com1C;
+
+        public override void Load(IServiceProvider serviceProvider)
+        {
+            // Сохраняем ссылку на контейнер сервисов
+            _serviceProvider = serviceProvider;
+
+         /*   COMConnector cntr = new COMConnector();
+            string conStr = @"Srvr=""172.16.243.20:1641""; Ref=""peo_2""; Usr=""IPS Техархив""; Pwd=""9875321"";";
+            com1C = cntr.Connect(conStr);
+            */
+            // Пример №6: Команда контекстного меню
+            CreateCommand();
+
+            base.Load(serviceProvider);
+        }
+
+        public override void Unload()
+        {
+            base.Unload();
+        }
+
+        internal void CreateCommand()
+        {
+            // Получаем ссылку на службу по регистрации расширений "Навигатора"
+            IFactory factory = ServicesManager.GetService(typeof(IFactory)) as IFactory;
+
+            // Получаем ссылку на сервис именованных значков
+            INamedImageList _images = _serviceProvider.GetService(typeof(INamedImageList)) as INamedImageList;
+
+            factory.ContextMenuTemplate.Nodes.Add(new MenuTemplateNode("LoadGPZ",
+                            "Сформировать заявку на включение в ГПЗ",
+                            _images.ImageIndex("imgSamples.CopyObject"),
+                            20,
+                            Int32.MaxValue));
+
+            // Регистрируем провайдер команд контекстного меню для категории "Версии объектов"
+            factory.AddCommandsProvider(Consts.CategoryObjectVersion, new LoadFrom1CCommandProvider());
+        }
+
+
+    }
+
+    internal class LoadFrom1CCommandProvider : ICommandsProvider
+    {
+
+        //internal static dynamic com1C;
+
+        /* public LoadFrom1CCommandProvider(dynamic comObj)
+         {
+             com1C = comObj;
+         }*/
+
+        public CommandsInfo GetMergedCommands(ISelectedItems items, IServiceProvider viewServices)
+        {
+            // ВНИМАНИЕ! Основное требование к данному методу – нельзя выполнять обращения к базе даных 
+            // для того, чтобы проверить, можно ли отображать команду меню или нет!
+
+            // Список добавленных или перекрытых команд контекстного меню
+            CommandsInfo commandsInfo = new CommandsInfo();
+
+            // Есть один выделенный элемент
+            if (items != null && items.Count == 1)
+            {
+                // Пробуем получить описание выделенного объекта
+                IDBTypedObjectID objID = items.GetItemData(0, typeof(IDBTypedObjectID)) as IDBTypedObjectID;
+                //   MessageBox.Show(objID.Caption);
+                // Выделен объект
+                if (objID != null)
+                {
+                    SessionKeeper keeper = new SessionKeeper();
+                    IDBObjectType objectType = keeper.Session.GetObjectType(objID.ObjectType);
+                    /*  while (objectType.ParentTypeID > 0)
+                      {
+                          objectType = keeper.Session.GetObjectType(objectType.ParentTypeID);
+                      }
+                      */
+                    // Можем добавить команду 
+                    if (objectType.ObjectTypeName == "Поручение на закупку")
+                    {
+                        commandsInfo.Add("LoadGPZ",
+                       new CommandInfo(TriggerPriority.ItemCategory,
+                       new ClickEventHandler(LoadFrom1CCommandProvider.CommandClick)));
+                        
+                    }
+                  
+
+                }
+
+            }
+
+            // Вернём список
+            return commandsInfo;
+        }
+
+        public CommandsInfo GetGroupCommands(ISelectedItems items, IServiceProvider viewServices)
+        {
+            // ВНИМАНИЕ! Основное требование к данному методу – нельзя выполнять обращения к базе даных 
+            // для того, чтобы проверить, можно ли отображать команду меню или нет!
+
+            // Список добавленных или перекрытых команд контекстного меню
+            CommandsInfo commandsInfo = new CommandsInfo();
+
+            // Вернём список
+            return commandsInfo;
+        }
+
+        internal static void CommandClick(ISelectedItems items, IServiceProvider viewServices, object additionalInfo)
+        {
+            
+            // Пробуем получить описание выделенного объекта
+            IDBTypedObjectID objID = items.GetItemData(0, typeof(IDBTypedObjectID)) as IDBTypedObjectID;
+            using (SessionKeeper keeper = new SessionKeeper())
+            {
+                // Выделен объект
+                if (objID == null) return;
+                // SessionKeeper keeper = new SessionKeeper();
+                NumByWords numwords = new NumByWords();
+
+                IDBRelationCollection relationCollection = keeper.Session.GetRelationCollection(MetaDataHelper.GetRelationTypeID(new Guid("cadd927b-306c-11d8-b4e9-00304f19f545")));
+                IDBObject objZZ = keeper.Session.GetObject(objID.ObjectID);
+                if (objZZ.LCStep != 10035)
+                {
+                    MessageBox.Show("Невозможно сформировать ГПЗ, так как закупочная заявка не подписана");
+                    return;
+                }
+
+                if (objZZ.ProjectID != 0)
+                {
+                    IDBProjectObject objProj = keeper.Session.GetObject(objZZ.ProjectID) as IDBProjectObject;
+                    
+
+                   // if (objProj != null)
+                    //{
+                    //Проверка участник ли пользователь или нет.
+                        if (objProj.IsProjectManager(keeper.Session.UserID) || objProj.IsProjectParticipant(keeper.Session.UserID))
+                        {
+                        }
+                        else
+                        {
+                            MessageBox.Show("Вы не являетесь учасником проекта! Создание невозможно" );
+                            return;
+                        }
+                    //}
+                }
+
+
+
+                // Получаем у системного контейнера сервисов ссылку на службу по созданию новых объектов
+                IObjectCreatorService creatorService = ServicesManager.GetService(typeof(IObjectCreatorService)) as IObjectCreatorService;
+               
+                //IObjectsChangingAnalyzer analyzerService = ServicesManager.GetService(typeof(IObjectsChangingAnalyzer)) as IObjectsChangingAnalyzer;
+                // analyzerService.Analyze(keeper.Session,null);
+                // Вызываем мастер по созданию новых объектов, получаем идентификатор версии нового объекта
+                Int64 newObjectID = creatorService.CreateObjectByTypeDialog(1998);
+                
+                // Если объект не был создан - выход
+                if (newObjectID == Intermech.Consts.UnknownObjectId || newObjectID == -1) return;
+         
+               // analyzerService.Analyze(keeper.Session, co);
+                relationCollection.Create(objID.ObjectID, newObjectID);
+                // Получаем ссылку на службу ведомлений
+                INotificationService svc = ServicesManager.GetService(typeof(INotificationService)) as INotificationService;
+                // Проверим её наличие
+                if (svc == null) return;                
+                DBObjectsEventArgs oe = new DBObjectsEventArgs(NotificationEventNames.ObjectsCreated, newObjectID);
+                IDBObject obj = keeper.Session.GetObject(newObjectID);
+                if (objZZ.ProjectID.ToString() != null || objZZ.ProjectID.ToString() != String.Empty)
+                    obj.ProjectID = objZZ.ProjectID;
+                              
+                if (obj != null)
+                {
+
+                    obj.GetAttributeByName("Ссылка на заявку").Value = objZZ.ObjectID;
+                }
+                else
+                {
+                    MessageBox.Show("Случилось невероятное");
+                }
+                
+              
+                string[] decnum = obj.GetAttributeByName("Сумма ГПЗ").AsString.Split(',');
+                
+                if (decnum[1].ToString() == String.Empty)
+                {
+                    obj.GetAttributeByName("Сумма ГПЗ").AsString += "00";
+
+                }
+               
+                decimal convertsum = Convert.ToDecimal(obj.GetAttributeByName("Сумма ГПЗ").AsString.Replace(" ","0"));
+                decimal nds = (convertsum/100)*20;
+                obj.GetAttributeByName("Сумма ТКП форматированная").Value = numwords.RurPhrase(convertsum).ToString() + "), НДС 20% " + nds.ToString() +"("
+                    + numwords.RurPhrase(nds).ToString();
+
+                svc.FireEvent(newObjectID, oe);
+
+
+                ICaptureFileChangesService cfs = ServicesManager.GetService(typeof(ICaptureFileChangesService)) as ICaptureFileChangesService;
+                cfs.CaptureChanges(newObjectID, SaveChangesMode.Checkin, null);
+                    
+    
+                obj.CheckIn();
+
+                Intermech.Navigator.DBObjects.Descriptor descriptor = new Intermech.Navigator.DBObjects.Descriptor(obj.ObjectID);
+              
+                // Пробуем открыть вновь созданный объект в новом окне "Навигатора"
+                Intermech.Navigator.Utils.OpenNewWindow(descriptor, null);
+           
+                keeper.Dispose();
+            }
+             
+
+        }
+
+
+    }
+
+}
